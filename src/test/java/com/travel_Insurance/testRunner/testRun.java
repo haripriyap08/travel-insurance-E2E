1d3dd57a81f50b3a87f6d629087c6866afb40415
haripriyap08 <101901617+haripriyap08@users.noreply.github.com>
2024-03-04 22:43:09 +0530

    Update testRun.java

--- src/test/java/com/travel_Insurance/testRunner/testRun.java
+++ src/test/java/com/travel_Insurance/testRunner/testRun.java
@@ -12,12 +12,12 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 //@RunWith(Cucumber.class)
-@CucumberOptions(features= {"src/test/resources/featureFile/policyBazaarE2E.feature"},																// Specifying the location of the feature file(s) that contain Gherkin syntax
-		glue= {"com.travel_Insurance.stepDefinitions"} ,																				// Specifying the package containing step definitions
-	        plugin={"pretty","html:Cucumberreport/myreport.html","com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}, // Plugins for generating different types of reports
-		 dryRun=false,
-		 monochrome=true,
-		publish=true
+@CucumberOptions(features= {"src/test/resources/featureFile/"},																// Specifying the location of the feature file(s) that contain Gherkin syntax
+				 glue= {"com.travel_Insurance.stepDefinitions"} ,																				// Specifying the package containing step definitions
+				 plugin={"pretty","html:Cucumberreport/myreport.html","com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}, // Plugins for generating different types of reports
+				dryRun=false,
+				monochrome=true,
+				publish=true
 		)
 
 //The class extending AbstractTestNGCucumberTests to run Cucumber with TestNG
